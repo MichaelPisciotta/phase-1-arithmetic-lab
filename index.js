cd9e0a60612c0578c2e93ead5c06541f663a5496
MichaelPisciotta <mfp56@miami.edu>
2021-08-02 16:32:59 -0400

    commit tree 1f65a2212dfc43a4f3d137640128d9e858ad7c45

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ let num2 = 2;
 let multiply = num1 * num2;
 
 
-
 let max = Math.max(1, 2, 3, 4, 20);
 
 
@@ -11,4 +10,18 @@ let num3 = 22;
 let num4 = 6;
 let mod = num3 % num4;
 
-let random = Math.floor(Math.random() * 10) + 1;
+
+let random = Math.floor(Math.random() * 10) + 0;
+
+
+
+
+
+
+
+
+
+
+
+
+// let random = Math.floor(Math.random() * 10) + 1;
